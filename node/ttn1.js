7cd93083445f74683aff2c00ba9db2179ef27fe0
Robert Oostenveld <r.oostenveld@gmail.com>
2016-09-11 10:20:16 +0200

    use a pug view for the index page

--- node/ttn1.js
+++ node/ttn1.js
@@ -63,7 +63,7 @@ function handleError(res, reason, message, code) {
 }
 
 app.get("/", function(req, res) {
-  res.send('endpoints are /all and /last');
+  res.render('index');
 });
 
 app.get("/all", function(req, res) {
