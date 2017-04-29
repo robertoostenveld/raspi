bd9daf1bee02accf8b1226f2f6ee96e32f2b3754
Robert Oostenveld <r.oostenveld@gmail.com>
2017-04-29 13:20:16 +0200

    moved hello to port 3008

--- node/hello.js
+++ node/hello.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello world!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3008, function () {
+  console.log('Example app listening on port 3008!');
 });
