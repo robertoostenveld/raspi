be89b52fc4ff1930fde8621ebf023d800370c26c
Robert Oostenveld <r.oostenveld@gmail.com>
2018-05-10 23:26:55 +0200

    made the pin explicit

--- python/blink1.py
+++ python/blink1.py
@@ -1,13 +1,15 @@
 import wiringpi
 import time
 
+pin=3
+
 wiringpi.wiringPiSetup()
 
-wiringpi.pinMode(0,1)
+wiringpi.pinMode(pin,1)
 
 while True:
     time.sleep(0.5)
-    wiringpi.digitalWrite(0,1)
+    wiringpi.digitalWrite(pin,1)
     time.sleep(0.5)
-    wiringpi.digitalWrite(0,0)
+    wiringpi.digitalWrite(pin,0)
   
